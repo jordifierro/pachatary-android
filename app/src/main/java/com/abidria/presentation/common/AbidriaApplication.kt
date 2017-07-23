e564b65852f8ccdeba676a9ee33009e84d72705e
jordifierro <jordifierromulero@gmail.com>
2017-07-23 13:00:50 +0200

    Cache picasso images

--- app/src/main/java/com/abidria/presentation/common/AbidriaApplication.kt
+++ app/src/main/java/com/abidria/presentation/common/AbidriaApplication.kt
@@ -5,6 +5,8 @@ import com.abidria.data.common.injection.DataModule
 import com.abidria.presentation.common.injection.ApplicationComponent
 import com.abidria.presentation.common.injection.DaggerApplicationComponent
 import com.facebook.stetho.Stetho
+import com.jakewharton.picasso.OkHttp3Downloader
+import com.squareup.picasso.Picasso
 
 class AbidriaApplication : Application() {
 
@@ -14,6 +16,12 @@ class AbidriaApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
         Stetho.initializeWithDefaults(this);
+
+        val picassoBuilder = Picasso.Builder(this)
+        picassoBuilder.downloader(OkHttp3Downloader(this, Long.MAX_VALUE))
+        picassoBuilder.indicatorsEnabled(true)
+        Picasso.setSingletonInstance(picassoBuilder.build())
     }
 }
