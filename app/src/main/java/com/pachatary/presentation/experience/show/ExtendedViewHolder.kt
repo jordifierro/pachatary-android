203b54d2e4f3a701a337d5f19fe53af49301bb49
jordifierro <jordifierromulero@gmail.com>
2018-04-26 21:30:15 +0200

    Show saves counter on explore experiences list

--- app/src/main/java/com/pachatary/presentation/experience/show/ExtendedViewHolder.kt
+++ app/src/main/java/com/pachatary/presentation/experience/show/ExtendedViewHolder.kt
@@ -17,6 +17,7 @@ class ExtendedViewHolder(view: View, val onClick: (String) -> Unit)
     private val descriptionView: TextView = view.findViewById(R.id.experience_description)
     private val pictureView: ImageView = view.findViewById(R.id.experience_picture)
     private val progressBar: ProgressBar = view.findViewById(R.id.experience_progressbar)
+    private val savesCountView: TextView = view.findViewById(R.id.experience_saves_count)
     lateinit var experienceId: String
 
     init { view.setOnClickListener(this) }
@@ -26,12 +27,14 @@ class ExtendedViewHolder(view: View, val onClick: (String) -> Unit)
         pictureView.visibility = View.VISIBLE
         authorView.visibility = View.VISIBLE
         descriptionView.visibility = View.VISIBLE
+        savesCountView.visibility = View.VISIBLE
         progressBar.visibility = View.GONE
 
         this.experienceId = experience.id
         titleView.text = experience.title
         authorView.text = "by " + experience.authorUsername
         descriptionView.text = experience.description
+        savesCountView.text = experience.savesCount.toString()
         Picasso.with(pictureView.context)
                 .load(experience.picture?.mediumUrl)
                 .into(pictureView)
@@ -42,6 +45,7 @@ class ExtendedViewHolder(view: View, val onClick: (String) -> Unit)
         pictureView.visibility = View.GONE
         authorView.visibility = View.GONE
         descriptionView.visibility = View.GONE
+        savesCountView.visibility = View.GONE
         progressBar.visibility = View.VISIBLE
     }
 
