c3b3a78fb1ec449ae6ac956f47c5c227e0d0e990
jordifierro <jordifierromulero@gmail.com>
2018-05-24 14:42:21 +0200

    Link playstore link on navigate to upgrade app

--- app/src/main/java/com/pachatary/presentation/main/MainActivity.kt
+++ app/src/main/java/com/pachatary/presentation/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.pachatary.presentation.main
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
@@ -150,7 +151,14 @@ class MainActivity : AppCompatActivity(), MainView {
     }
 
     override fun navigateToUpgradeApp() {
-        //TODO
+        val appPackageName = packageName
+        try { startActivity(Intent(Intent.ACTION_VIEW,
+                                   Uri.parse("market://details?id=$appPackageName")))
+        } catch (anfe: android.content.ActivityNotFoundException) {
+            startActivity(Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=$appPackageName")))
+        }
+
         finish()
     }
 
