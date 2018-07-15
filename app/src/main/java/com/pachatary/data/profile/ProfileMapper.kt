9cd702ac778edbd700e8bd210d59caf8aeafb7d5
jordifierro <jordifierromulero@gmail.com>
2018-07-15 20:23:39 +0200

    Fix profile picture mapping error with proguard

--- app/src/main/java/com/pachatary/data/profile/ProfileMapper.kt
+++ app/src/main/java/com/pachatary/data/profile/ProfileMapper.kt
@@ -1,11 +1,12 @@
 package com.pachatary.data.profile
 
 import com.pachatary.data.common.ToDomainMapper
-import com.pachatary.data.picture.LittlePicture
+import com.pachatary.data.picture.LittlePictureMapper
 
 data class ProfileMapper(val username: String, val bio: String,
-                         val picture: LittlePicture?, val isMe: Boolean) : ToDomainMapper<Profile> {
+                         val picture: LittlePictureMapper?, val isMe: Boolean)
+                                                                         : ToDomainMapper<Profile> {
 
     override fun toDomain() = Profile(username = this.username, bio = this.bio,
-                                      picture = this.picture, isMe = this.isMe)
+                                      picture = this.picture?.toDomain(), isMe = this.isMe)
 }
