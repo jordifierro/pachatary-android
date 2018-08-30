6c684432ae07ce5c12917f25e7ccb654aefb7c84
jordifierro <jordifierromulero@gmail.com>
2018-08-30 19:01:06 +0200

    Fix move map center problems when zoom far

--- app/src/main/java/com/pachatary/presentation/common/edition/SelectLocationView.kt
+++ app/src/main/java/com/pachatary/presentation/common/edition/SelectLocationView.kt
@@ -4,7 +4,7 @@ import io.reactivex.Flowable
 
 interface SelectLocationView {
     enum class ZoomLevel(val zoom: Double) {
-        FAR(0.0), MID(13.0), NEAR(18.0)
+        FAR(1.0), MID(13.0), NEAR(18.0)
     }
     fun setInitialLocation(latitude: Double, longitude: Double, zoomLevel: ZoomLevel)
     fun latitude(): Double?
