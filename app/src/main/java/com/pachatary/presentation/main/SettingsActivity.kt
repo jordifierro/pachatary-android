46e90f8f8c4a56388e658a24e085111e6d814594
jordifierro <jordifierromulero@gmail.com>
2018-09-20 19:06:32 +0200

    Add feature to send feedback email

--- app/src/main/java/com/pachatary/presentation/main/SettingsActivity.kt
+++ app/src/main/java/com/pachatary/presentation/main/SettingsActivity.kt
@@ -2,6 +2,7 @@ package com.pachatary.presentation.main
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
@@ -9,6 +10,7 @@ import android.widget.Button
 import android.widget.ProgressBar
 import android.widget.RelativeLayout
 import android.widget.Toast
+import com.pachatary.BuildConfig
 import com.pachatary.R
 import com.pachatary.presentation.common.PachataryApplication
 import com.pachatary.presentation.common.view.ToolbarUtils
@@ -32,6 +34,14 @@ class SettingsActivity : AppCompatActivity() {
             .setOnClickListener { startActivity(WebViewActivity.newPrivacyPolicyIntent(this)) }
         findViewById<RelativeLayout>(R.id.settings_terms_button)
             .setOnClickListener { startActivity(WebViewActivity.newTermsAndConditionsIntent(this)) }
+        findViewById<RelativeLayout>(R.id.feedback_button)
+                .setOnClickListener {
+                    val emailIntent = Intent(Intent.ACTION_SENDTO, Uri.fromParts(
+                            "mailto", BuildConfig.FEEDBACK_EMAIL, null))
+                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN &&
+                            android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2)
+                        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(BuildConfig.FEEDBACK_EMAIL))
+                    startActivity(Intent.createChooser(emailIntent, ""))}
     }
 
     override fun onSupportNavigateUp(): Boolean {
